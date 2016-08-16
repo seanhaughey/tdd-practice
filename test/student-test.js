68013dd5ea4e8df1ca107e6a36c2b5f46e977018
Sean Haughey <sdhaughey@gmail.com>
2016-08-16 12:39:56 -0500

    refactored test file to ES6

--- test/student-test.js
+++ test/student-test.js
@@ -5,39 +5,39 @@ const assert = chai.assert;
 
 const Student = require('../src/student');
 
-describe('Student', function () {
+describe('Student', () => {
 
-  describe('.new', function () {
+  describe('.new', () => {
     const shirish = new Student('Shirish', 'Dali', 1000000);
-    it('should set #firstName', function () {
+    it('should set #firstName', () => {
       assert.equal(shirish.firstName, 'Shirish');
     });
 
-    it('should set #lastName', function () {
+    it('should set #lastName', () => {
       assert.equal(shirish.lastName, 'Dali');
     });
 
-    it('should set #salary', function () {
+    it('should set #salary', () => {
       assert.equal(shirish.salary, 1000000);
     });
   });
 
-  describe('#fullName()', function () {
-    it('should return #firstName and #lastName concatenated', function () {
+  describe('#fullName()', () => {
+    it('should return #firstName and #lastName concatenated', () => {
       const jesse = new Student('Jesse', 'Houghton', 1000000000);
       assert.equal(jesse.fullName(), 'Jesse Houghton');
     });
   });
 
-  describe('#formattedSalary()', function () {
-    it('should convert #salary into a currency', function () {
+  describe('#formattedSalary()', () => {
+    it('should convert #salary into a currency', () => {
       const wes = new Student('Wes', 'Hoover', 70000);
       assert.equal(wes.formattedSalary(), '$70,000.00');
     });
   });
 
-  describe('#giveRaise()', function () {
-    it('should increment #salary by 10%', function () {
+  describe('#giveRaise()', () => {
+    it('should increment #salary by 10%', () => {
       let richard = new Student('Richard', 'Davis', 70000);
       richard.giveRaise();
       assert.equal(richard.salary, 77000);
