7b385ad8fcadf0c17a9c37b910d7a3d62cf1b71b
Sean Haughey <sdhaughey@gmail.com>
2016-08-16 11:07:58 -0500

    first test passing

---
+++ src/student.js
@@ -0,0 +1,7 @@
+function Student(fn, ln, sal) {
+  this.firstName = fn;
+  this.lastName = ln;
+  this.salary = sal
+};
+
+module.exports = Student;
--- test/student-test.js
+++ test/student-test.js
@@ -1,1 +1,21 @@
-const mocha = require("mocha");
+const chai = require('chai');
+const assert = chai.assert;
+
+const Student = require('../src/student');
+
+describe('Student', function () {
+  describe('.new', function () {
+    const shirish = new Student('Shirish', 'Dali', 1000000);
+    it('should set #firstName', function () {
+      assert.equal(shirish.firstName, 'Shirish');
+    });
+
+    it('should set #lastName', function () {
+      assert.equal(shirish.lastName, 'Dali');
+    });
+
+    it('should set #salary', function () {
+      assert.equal(shirish.salary, 1000000);
+    })
+  });
+});
